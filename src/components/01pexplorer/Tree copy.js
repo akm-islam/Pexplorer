67f0ae780549511fd49056ac7fa9d013438af083
Mamun <mamun@Mamuns-Mac-mini.local>
2020-06-04 02:42:02 -0400

    random

--- src/components/01pexplorer/Tree copy.js
+++ src/components/01pexplorer/Tree copy.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import './css/tree.css';
-import { group } from 'd3';
 
 class Tree extends Component {
   constructor(props) {
@@ -25,24 +24,80 @@ createTree(mytreedata,mywidth,myheight){
 //------------------------- Initial set ups
 var margin = {top: 40, right: mywidth*.01, bottom: 0, left: mywidth*.01},
 width = mywidth - margin.right - margin.left,
-height = 350 - margin.top - margin.bottom;
-var rectConfig={width:500,height:30}
-//-----
-var svg = d3.select(this.node).append("svg").attr("width", width + margin.right + margin.left).attr("height", height + margin.top + margin.bottom).attr("id","svg_container")
-var groups=svg.selectAll('g').append('g').data([0,1,2,3])
-groups.exit().remove()
-var groupsEnter=groups.enter().append("g").attr('transform',(d,i)=>'translate(' + 0 + ',' + 0 + ')')
-groups.merge(groupsEnter)
-var text=groupsEnter.append('text').text("hello").attr('y',(d,i)=>height-80).attr('x',(d,i)=>i*(rectConfig.width/4)+130)
-var line= groupsEnter.append('line')
-     .attr('x1',(d,i)=>i*(rectConfig.width/4)+145)
-     .attr('y1', 30)
-     .attr('x2',(d,i)=>i*(rectConfig.width/4)+145)
-     .attr('y2', height-100)
-     .attr("stroke-width",5)
-     .style('stroke', 'red');
+height = 350 - margin.top - margin.bottom,
+dataSource = treeData[0],
+i = 0;
+console.log(mywidth)
+/*
+if(mywidth<570){
+  width = 560 - margin.right - margin.left;
+}*/
 
-svg.append('rect').attr("width",rectConfig.width).attr('height',30).attr('x',100).attr('y',25).attr('fill','green')
+//------------------------- Generate the treeLayout and the Diagonal 
+var treeLayout = d3.layout.tree().size([width, height]);
+var diagonal = d3.svg.diagonal().projection(function(d) { return [d.x, d.y]; });
+var svg = d3.select(this.node).append("svg") // select the node
+.attr("width", width + margin.right + margin.left).attr("height", height + margin.top + margin.bottom).attr("id","svg_container")
+.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var nodes = treeLayout.nodes(dataSource),
+links = treeLayout.links(nodes);
+nodes.forEach(function(d) { d.y = d.depth * 230; }); // Normalize for fixed-depth
+//------------------------- Declare the nodes and Enter the nodes
+var node = svg.selectAll("g.node").data(nodes, function(d) { return d.id || (d.id = ++i); }); // attach data to nodes
+var nodeEnter = node.enter().append("g").attr("class", "node").attr("transform", function(d) { 
+return "translate(" + d.x + "," + d.y + ")"; });
+
+//-----------Circle and percentage
+nodeEnter.append("circle")
+//.attr("r",(d)=> 0).transition().duration(2000)
+.attr("r",(d)=> d.percentage!='Traits'?22:26).attr("fill",(d)=>d.percentage==highest_value?"#F7882F":"#4ceebd").attr("id",(d)=>d.percentage=='Traits'?"p_circle":"mycircle");
+//-----Enter text
+nodeEnter.append("text").attr("y", 0).attr("dy", ".35em").attr("text-anchor", "middle")
+.text(function(d) { 
+  if(d.percentage!="Traits"){
+    return d.percentage+"%";
+  }
+  else{
+    return d.percentage;
+  }
+  
+
+})
+//.style("font","0px sans-serif").transition().duration(2000)
+.style("font","12px sans-serif").style("fill-opacity", 1).attr("class",(d)=>{
+if(d.percentage=="Traits"){
+  return "parent"
+}
+else{
+  return "text1";
+}
+});
+//----------- Text for personality traits
+nodeEnter.append("text")
+   .attr("x", 0)
+   .attr("dy", ".35em")
+   .attr("y", 40)
+   .attr("text-anchor", "middle")
+   .text(function(d) { 
+     if(d.percentage!='Traits')return d.trait_name; 
+    })
+ //   attr("fill",(d)=>d.percentage==highest_value?"#f1a814":"#4ceebd")
+    .style("font",(d)=>d.percentage==highest_value?"9px sans-serif":"10px sans-serif")
+    .transition().duration(800).style(()=>{
+      if(width<570){
+        return "font","10px sans-serif"
+      }
+      else 
+      return "font","10px sans-serif";
+    })
+   .attr("class","text2");
+
+   svg.selectAll("g.node").data(nodes, function(d) { return d.id || (d.id = ++i); }).exit().remove()
+//------------------------- Declare  and enter the links
+var link = svg.selectAll("path.link").data(links, function(d) { return d.target.id; });
+link.enter().insert("path", "g").attr("class", "link")
+//.style("opacity",0).transition().duration(2000)
+.style("opacity",.5).attr("d", diagonal);
 }
 
 
